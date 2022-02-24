79c2c95955cbd7787152dc3f97aab96d3479a930
mdddj <413153189@qq.com>
2022-02-24 11:38:41 +0800

    new actions

--- lib/pages/index/index.dart
+++ lib/pages/index/index.dart
@@ -13,7 +13,23 @@ class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
     return MacosScaffold(
-      titleBar: TitleBar(title: Text('首页'),),
+      titleBar: const TitleBar(title: Text('首页'),),
+      children: [
+        ContentArea(builder: (BuildContext context, ScrollController scrollController) {
+          return ListView.builder(itemBuilder: (BuildContext context, int index) {
+            return Container();
+          },itemCount: 1,);
+        },),
+        ResizablePane(minWidth: 200,startWidth: 200, resizableSide: ResizableSide.left, builder: (BuildContext context, ScrollController scrollController) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                  Text('打赏')
+              ],
+            ),
+          );
+        },)
+      ],
     );
   }
 }
